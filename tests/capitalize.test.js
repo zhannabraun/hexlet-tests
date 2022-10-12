3896be05599d9dbee62d2032be6ac7eccd4dc3a7
zhannabraun <zhannabraun18@gmail.com>
2022-10-12 15:38:30 +0300

    Fix tests with assert module

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,5 @@
+import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
-
-console.log('Все тесты пройдены!');
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
